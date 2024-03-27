d5b2c6f8fb94a54dd333e2d65733713129f84c76
haseebvc <haseebsilat00@gmail.com>
2024-03-28 02:23:41 +0500

    second assginment

---
+++ main.js
@@ -0,0 +1,4 @@
+var uesr1 = "haseeb";
+var pasward1 = 1234;
+console.log(pasward1);
+console.log(uesr1);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let uesr1 = "haseeb"
+let pasward1 : number = 1234
+console.log (pasward1)
+console.log (uesr1)
